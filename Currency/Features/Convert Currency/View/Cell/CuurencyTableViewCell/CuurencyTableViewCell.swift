c898ceff0751a474d14bdb8b7432f4d8731255f8
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2022-07-06 17:09:40 +0200

    Code refactor

---
+++ Currency/Features/Convert Currency/View/Cell/CuurencyTableViewCell/CuurencyTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CuurencyTableViewCell.swift
+//  Currency
+//
+//  Created by Mohamed Samir on 06/07/2022.
+//
+
+import UIKit
+
+class CuurencyTableViewCell: UITableViewCell {
+    @IBOutlet weak var currencyLabel: UILabel!
+}
---
+++ Currency/Features/Details of Currency/View/Contollers/DetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.swift
+//  Currency
+//
+//  Created by Mohamed Samir on 06/07/2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
