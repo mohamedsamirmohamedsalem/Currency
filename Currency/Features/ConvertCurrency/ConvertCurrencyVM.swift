b8c4edf03e1262be5457db7f12dbace00b00048c
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2022-09-29 16:11:25 +0200

    subscribe on data coming from Api using Rxswift

--- Currency/Features/ConvertCurrency/ConvertCurrencyVM.swift
+++ Currency/Features/ConvertCurrency/ConvertCurrencyVM.swift
@@ -73,7 +73,6 @@ struct ConvertCurrencyVM {
         // view model observing for converting data
         repository?.convertCurrencyResponse.subscribe(onNext: {  convertCurrencyModel in
             self.convertCurrencyModel.onNext(convertCurrencyModel)
-            //repository.
         }).disposed(by: disposeBag)
         
     }
